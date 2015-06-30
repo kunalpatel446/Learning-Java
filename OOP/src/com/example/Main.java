5c7c5f9ab7ad3b68a4d64eb7007dfebcb8497d2f
Kunal Patel <kunalpatel446@gmail.com>
2015-06-30 18:57:33 -0400

    OOP not working

---
+++ OOP/src/com/example/Main.java
@@ -0,0 +1,12 @@
+package Java;
+import Java.OOP;
+public class Main
+{
+	public static void main(String[] args)
+	{
+		OOP b = new OOP("Hello there,");
+		OOP c = new OOP("my name is Kunal.");
+		b.call();
+		c.call();
+	}
+}
